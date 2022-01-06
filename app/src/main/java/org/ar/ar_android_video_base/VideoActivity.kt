88d9fcc11d70a6ed932f7d01f9cdab592e6af9cc
zxy <2544682494@qqcom>
2022-01-06 18:43:19 +0800

    change host

--- app/src/main/java/org/ar/ar_android_video_base/VideoActivity.kt
+++ app/src/main/java/org/ar/ar_android_video_base/VideoActivity.kt
@@ -40,7 +40,7 @@ class VideoActivity : AppCompatActivity(), View.OnClickListener {
         override fun onJoinChannelSuccess(channel: String, uid: String, elapsed: Int) {
             super.onJoinChannelSuccess(channel, uid, elapsed)
             CoroutineScope(Dispatchers.IO).launch {
-                Fuel.post("http://192.168.16.254:8080/cloudRecord/start")
+                Fuel.post("http://rtcback.zhiyashengya.com/cloudRecord/start")
                     .objectBody(StartRecordRequest(channel, uid))
                     .response { _, response, _ ->
                         if (response.isSuccessful) {
